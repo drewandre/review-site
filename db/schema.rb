ca4b1d9a15def9bcd7302db0dc6d37d64dfd847f
Zach Stone <hexaphixylix@yahoo.com>
2017-10-17 14:54:37 -0400

    add omniauth for github

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017140323) do
+ActiveRecord::Schema.define(version: 20171017151426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "login", null: false
+    t.string "email"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -28,7 +29,8 @@ ActiveRecord::Schema.define(version: 20171017140323) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "provider"
+    t.string "uid"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
