51a8d7ac1c3cfd58e2a89ae1ee250cfeeedf3ff4
Marcus-boyd <marcus.boyd389@gmail.com>
2017-10-19 10:07:25 -0400

    fix login function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.login = auth.info.nickname
-      user.email = auth.info.email
+      user.email = auth.info.email || "none"
       user.password = Devise.friendly_token[0,20]
     end
   end
